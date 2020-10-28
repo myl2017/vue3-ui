42760004640e42c6bad9fb2f2ca0a55ed758cd76
mengyl <706695013@qq.com>
2020-10-28 13:28:11 +0800

    创建 views模块

---
+++ src/views/Doc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Doc 页面
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Doc'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Home 页面
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
